63dfbcec8132320ed531571c1daf4aa6e5dd8aa1
Nenouchka <helene.morel@gmail.com>
2020-05-06 19:12:38 +0200

    bug fixed

---
+++ src/components/Counter.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function Counter() {
+
+  const [count, setCount] = useState(0);
+
+  const handleChange = event => setCount(event.target.value)
+
+  return (
+    <div>
+      <input type='number' value={count} onChange={handleChange} />
+      <p>Le compteur est  à : {count} </p>
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
